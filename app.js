d69ba43c6bb85238e1129b2aa14dbebc69fb51ab
codewithemmy <codewithemmy@gmail.com>
2022-12-03 12:52:01 +0000

    swagger re-edit

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ require("dotenv").config();
 require("express-async-errors");
 
 // extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 //require cloudinary version 2
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 //require express
 const express = require("express");
@@ -42,18 +42,23 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //use extra security packages
 app.use(
-    rateLimiter({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
-    })
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
 );
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(xss());
 
+//create a root directory and link to swagger documentation
+app.get("/", (req, res) => {
+  res.send('<h1>WE BARTER API DOCS</h1><a href="/api-docs">Documentation</a>');
+});
 
-app.get('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
@@ -67,14 +72,14 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8800;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL);
-        app.listen(port, () =>
-            console.log(`Server is listening on port ${port}...`)
-        );
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
